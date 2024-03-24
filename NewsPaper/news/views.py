09de2f70a3b75387b4f6c6e66100bcd1da701109
Sergey Glavachuk <arh_cdp@mail.ru>
2024-03-24 12:44:29 +0300

    HW_24/03

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -2,18 +2,25 @@ from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 from .models import Post
 from datetime import datetime
+from .filters import PostFilter
 # Create your views here.
 class PostList(ListView):
     model = Post
     ordering = 'date'
     template_name = 'flatpages/news.html'
     context_object_name = 'news'
+    paginate_by = 2
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['time_now'] = datetime.utcnow()
         context['next_sale'] = None
         return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
 
 class DetailPost(DetailView):
     model = Post
